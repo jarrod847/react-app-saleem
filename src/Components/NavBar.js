c71aed2aea3881bcaeb972e98d336293995efcec
jarrodskahill <jarrodskahill@gmail.com>
2020-12-07 15:55:00 -0600

    creating react app and react-router-dom

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="Navbar">
+      <h1>Saleem's Place</h1>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/Components/homePage/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Hi, this is my website</h2>
+    </div>
+  );
+};
+
+export default HomePage;
